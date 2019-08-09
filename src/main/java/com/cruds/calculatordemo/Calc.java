f27e5c8b761703bbb7d5e4699d5e93fa982dcc90
praveen1693 <krishna.praveen16@gmail.com>
2019-08-09 12:28:22 +0530

    calcdemo second commit

--- src/main/java/com/cruds/calculatordemo/Calc.java
+++ src/main/java/com/cruds/calculatordemo/Calc.java
@@ -13,4 +13,13 @@ public class Calc {
 		return x-y;
 	}
 
+	public int multiply(int x, int y)
+	{
+		return x*y;
+	}
+	
+	public int divide(int x, int y)
+	{
+		return x/y;
+	}
 }
--- src/test/java/com/cruds/calculatordemo/TestCalc.java
+++ src/test/java/com/cruds/calculatordemo/TestCalc.java
@@ -20,4 +20,17 @@ public class TestCalc {
 		assertEquals(20, c.sub(25, 5));
 	}
 	
+	@Test
+	public void testMultiply() {
+		Calc c = new Calc();
+		
+		assertEquals(25, c.multiply(5, 5));
+	}
+	
+	@Test
+	public void testDivide() {
+		Calc c = new Calc();
+		
+		assertEquals(5, c.divide(25, 5));
+	}
 }
